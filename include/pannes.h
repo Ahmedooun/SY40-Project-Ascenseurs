12c469ca2efc9eaaf128a009d2678d14d12fd96b
Ahmedooun <ahmed-ounissi@hotmail.com>
2026-01-01 13:56:44 +0100

    Update pannes.h

--- include/pannes.h
+++ include/pannes.h
@@ -1,1 +1,21 @@
+#ifndef PANNES_H
+#define PANNES_H
 
+#include "ipc.h"
+#include "common.h"
+#include <pthread.h>
+
+typedef struct {
+    const IPC *ipc;
+    Ascenseur *asc;
+    pthread_mutex_t *asc_mtx;
+    unsigned int seed;
+    int min_s;
+    int max_s;
+    int duree_panne_s;
+    int stop;
+} PannesArgs;
+
+void* pannes_thread(void *arg);
+
+#endif
