d9730d70d576b92ef5af977d26b924f4e57b95ba
Ahmedooun <ahmed-ounissi@hotmail.com>
2026-01-10 08:22:42 +0100

    Update interface.h

--- include/interface.h
+++ include/interface.h
@@ -1,1 +1,23 @@
+#ifndef INTERFACE_H
+#define INTERFACE_H
 
+#include <pthread.h>
+#include "usagers.h"
+#include "common.h"
+
+typedef struct {
+    FileDemandes *q;
+
+    Ascenseur *asc;
+    int nb_asc;
+    pthread_mutex_t *asc_mtx;
+
+    int *stop_global;
+
+    // 1 = auto (call désactivé), 2 = manuel (call autorisé)
+    int mode;
+} InterfaceArgs;
+
+void* interface_thread(void *arg);
+
+#endif
